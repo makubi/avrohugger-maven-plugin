5de6be3b76f473b8e172481aa6a20c957400e3f9
Mathias Kub <git@makubi.at>
2017-07-01 08:23:46 +0200

    Update test records to match generated classes
    See
    https://github.com/julianpeeters/avrohugger/commit/42df0facc4271615f79202643ec619dd5d1b89cc.

--- src/test/resources/unit/avrohugger-maven-plugin/expected/Record.scala
+++ src/test/resources/unit/avrohugger-maven-plugin/expected/Record.scala
@@ -7,7 +7,7 @@ case class Record(var text: String) extends org.apache.avro.specific.SpecificRec
   def this() = this("")
   def get(field$: Int): AnyRef = {
     (field$: @switch) match {
-      case pos if pos == 0 => {
+      case 0 => {
         text
       }.asInstanceOf[AnyRef]
       case _ => new org.apache.avro.AvroRuntimeException("Bad index")
@@ -15,7 +15,7 @@ case class Record(var text: String) extends org.apache.avro.specific.SpecificRec
   }
   def put(field$: Int, value: Any): Unit = {
     (field$: @switch) match {
-      case pos if pos == 0 => this.text = {
+      case 0 => this.text = {
         value.toString
       }.asInstanceOf[String]
       case _ => new org.apache.avro.AvroRuntimeException("Bad index")
